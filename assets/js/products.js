67d62f61e7a1d11d28c22a906492494ddad00bfe
PERFEXION <chubuike17@gmail.com>
2025-09-24 21:15:54 +0100

    updated the navigation and footer of the CART and CHECKOUT page

--- assets/js/products.js
+++ assets/js/products.js
@@ -14,19 +14,4 @@ const products = [
     sizeLimits: { min: 6, max: 16 },
     category: "Leggings",
   },
-  {
-    id: 2,
-    name: "Widget B",
-    price: 29990.0,
-    description: "An even better widget with extra features.",
-    images: [
-      "https://via.placeholder.com/200?text=Img1",
-      "https://via.placeholder.com/200?text=Img2",
-      "https://via.placeholder.com/200?text=Img3",
-      "https://via.placeholder.com/200?text=Img4",
-    ],
-    colors: ["black", "white"],
-    sizeLimits: { min: 5, max: 15 },
-    category: "Electronics",
-  },
 ];
